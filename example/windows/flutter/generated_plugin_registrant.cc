8734c93a646c1913be6c0d7e8f5b9f8906b47595
Minas <whiplashoo721@gmail.com>
2021-10-26 09:56:39 +0300

    adapted plugin for windows

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <window_to_front/window_to_front_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WindowToFrontPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowToFrontPlugin"));
+}
